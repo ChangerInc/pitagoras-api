6f3e6122157d24f1a56f4eb92657d92c1324d4ae
MateusjfSilva <mateus.jsilva@sptech.school>
2023-09-04 12:31:19 -0300

    Cors class criada e configurada para receber dados de outro endpoint

---
+++ projeto.api/src/main/java/com/changer/projeto/api/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.changer.projeto.api;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000"); // Permita o acesso a partir do seu domínio React
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter((CorsConfigurationSource) source);
+    }
+}
+
