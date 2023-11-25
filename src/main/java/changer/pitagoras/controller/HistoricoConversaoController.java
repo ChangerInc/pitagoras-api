f822bdaa3f0f6ca92ad0afd55a60b1c5b3716863
Matheu5Leone <matheus.lsilva@sptech.school>
2023-11-25 18:52:34 -0300

    FIX nome /historico-conversao

--- src/main/java/changer/pitagoras/controller/HistoricoConversaoController.java
+++ src/main/java/changer/pitagoras/controller/HistoricoConversaoController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/historicoconversao")
+@RequestMapping("/historico-conversao")
 public class HistoricoConversaoController {
 
     @Autowired
