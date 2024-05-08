86fba4ebda533bfe66fc787e23c31bb06f0b64a7
Matheu5Leone <matheus.lsilva@sptech.school>
2024-05-07 23:23:48 -0300

    getArquivos do usuario agora tras objetos com url

--- src/main/java/changer/pitagoras/controller/ArquivoController.java
+++ src/main/java/changer/pitagoras/controller/ArquivoController.java
@@ -3,11 +3,13 @@ package changer.pitagoras.controller;
 import changer.pitagoras.model.Arquivo;
 import changer.pitagoras.service.ArquivoService;
 import changer.pitagoras.service.S3Service;
+import changer.pitagoras.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -17,6 +19,8 @@ public class ArquivoController {
     private ArquivoService service;
     @Autowired
     private S3Service s3Service;
+    @Autowired
+    private UsuarioService usuarioService;
 
     @PostMapping("/{idUsuario}")
     public ResponseEntity<Arquivo> criarArquivo(@RequestBody MultipartFile file, @PathVariable UUID idUsuario) {
@@ -29,5 +33,15 @@ public class ArquivoController {
         Arquivo arquivo = service.buscarArquivo(idArquivo);
         String nomeArquivo = arquivo.getNome();
         return ResponseEntity.status(200).body(s3Service.downloadArquivo(nomeArquivo, idUsuario));
+    }
+
+    @GetMapping("/{idUsuario}")
+    public ResponseEntity<List<Arquivo>> getArquivosById(
+            @PathVariable UUID idUsuario) {
+        List<Arquivo> arqs = usuarioService.resgatarArquivos(idUsuario);
+
+        return arqs.isEmpty()
+                ? ResponseEntity.status(204).build()
+                : ResponseEntity.status(200).body(arqs);
     }
 }
