5e56c1fc6ce2f9cef738afada36a067fbbb94d98
LeonardoLopesPrado <111136996+LeonardoLopesPrado@users.noreply.github.com>
2024-06-05 21:29:09 -0300

    Alterado ReactConnector

--- src/main/java/changer/pitagoras/config/ReactConnector.java
+++ src/main/java/changer/pitagoras/config/ReactConnector.java
@@ -14,8 +14,10 @@ public class ReactConnector {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://changer.serveftp.com/"); // Permita o acesso a partir do seu domínio React
-        config.addAllowedOrigin("http:/44.217.150.7/"); // Permita o acesso a partir do seu domínio React
+        config.addAllowedOrigin("https://changer.serveftp.com/");
+        config.addAllowedOrigin("http://changer.serveftp.com/");
+        config.addAllowedOrigin("http://44.217.150.7/");
+        config.addAllowedOrigin("http://localhost:5173/"); // Permita o acesso a partir do seu domínio React
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
         source.registerCorsConfiguration("/**", config);
